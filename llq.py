698dd751ef03f991d114fa15c54ba3cb35d66c90
vshekn <vshekn@gmail.com>
2018-04-21 14:05:51 +0300

    some fixes; strange behaviour

--- llq.py
+++ llq.py
@@ -62,37 +62,37 @@ class SiteStatus:
 def distance(x1, y1, x2, y2):
     return ((x1-x2)**2+(y1-y2)**2)**0.5
         
-class SitesWrapper:
-    def __init__(self, sites):
-        self.as_list = sites
-        self.sites = {}
-        for site in sites:
-            self.sites[site.site_id] = site        
-
 class Unit:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+def find_closest_among(sites, unit, sites_coord):
+    min_d = 200000
+    result = -1
+    for site in sites:
+        d = distance(unit.x, unit.y, sites_coord[site].x, sites_coord[site].y)
+        if d < min_d:
+            min_d = d
+            result = site
+    return site
+
 num_sites = int(input())
-sites = []
+sites_coord = {}
 for i in range(num_sites):
     site_id, x, y, radius = [int(j) for j in input().split()]
-    sites.append(Site(x, y, radius, site_id))
+    sites_coord[site_id] = Site(x, y, radius, site_id)
     
-wrapped = SitesWrapper(sites)
-
 # game loop
 while True:
     # touched_site: -1 if none
     gold, touched_site = [int(i) for i in input().split()]
     currents = {}
-    target = -1
-    min_d = 100000
     owned_knight_barracks = []
     owned_archer_barracks = []
     owned_towers = []
     empty_sites = []
+
     for i in range(num_sites):
         # ignore_1: used in future leagues
         # ignore_2: used in future leagues
@@ -100,46 +100,45 @@ while True:
         # owner: -1 = No structure, 0 = Friendly, 1 = Enemy
         site_id, ignore_1, ignore_2, structure_type, owner, param_1, param_2 = [int(j) for j in input().split()]
         currents[site_id] = SiteStatus(ignore_1, ignore_2, structure_type, owner, param_1, param_2)
+    	if structure_type == NO_STRUCTURE:
+            empty_sites.append(site_id)
         if owner == FRIENDLY:
             if structure_type == TOWER:
                 owned_towers.append(site_id)
-            elif structure_type == BARRACKS:
+            if structure_type == BARRACKS:
                 if param_1 == KNIGHT_BARRACKS:
                     owned_knight_barracks.append(site_id)
                 elif param_1 == ARCHER_BARRACKS:
                     owned_archer_barracks.append(site_id)
-        elif owner == EMPTY_SITE:
-            empty_sites.append(site_id)
     
     num_units = int(input())    
     for i in range(num_units):
         # unit_type: -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
         x, y, owner, unit_type, health = [int(j) for j in input().split()]
-        if unit_type == -1:
-            if owner == 0:
+        if unit_type == QUEEN:
+            if owner == FRIENDLY_UNIT:
                 own_queen = Unit(x, y)
             else:
                 enemy_queen = Unit(x, y)
 
-    min_d = 1000000
-    target = -1
-    build_list = []
-    for key, value in currents.items():
-        if value.owner != 0 and value.structure_type != 2:
-            site = wrapped.sites[key]
-            d = distance(own_queen.x, own_queen.y, site.x, site.y)
-            if d < min_d:
-                target = key
-                min_d = d
-        if value.owner == 0 and gold >= 80:
-            build_list.append(str(key))
-            gold -= 80
-    
-    if target == -1:
-        print('WAIT')
-    else:
-        print('BUILD {0} BARRACKS-KNIGHT'.format(target))
-    if len(build_list) == 0:
+
+    if len(owned_knight_barracks) == 0:
+        target = find_closest_among(empty_sites, own_queen, sites_coord)
+        if target != -1:
+            print('BUILD {0} BARRACKS-KNIGHT'.format(target))
+        else:
+            print('WAIT')
         print('TRAIN')
     else:
-        print('TRAIN {0}'.format(' '.join(build_list)))
+        if len(owned_towers) < 2:
+            target = find_closest_among(empty_sites, own_queen, sites_coord)
+            if target != -1:
+                print('BUILD {0} TOWER'.format(target))
+            else:
+                print('WAIT')
+        else:
+            print('BUILD {0} TOWER'.format(owned_towers[0]))
+        if gold >= KNIGHT_COST:
+            print('TRAIN {0}'.format(owned_knight_barracks[0]))
+        else:
+            print('TRAIN')
